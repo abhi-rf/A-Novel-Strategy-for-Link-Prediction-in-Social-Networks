753f378292c3a22cb115fe0907ed1e932dc3e622
Abhishek Saroha <abhishek.saroha67@gmail.com>
2018-01-07 00:07:54 +0530

    Implementation

---
+++ impl.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 23:08:40 2018
+
+@author: Dell
+"""
+
+import networkx as nx
+dolphin = "C:/Users/Dell/Downloads/dolphins/dolphins.gml" 
+karate = "C:/Users/Dell/Downloads/karate/karate.gml"
+
+if __name__ == "__main__":
+    #Finding prob between 0 and 5
+    G = nx.read_gml(dolphin, label='id')
+    #G = nx.read_gml(karate,label='id')
+    #print(G.nodes())
+    #print(list(G.neighbors(0)))
+    neighbors_x = set((G.neighbors(0)))
+    neighbors_y = set((G.neighbors(5)))
+    print(neighbors_x," ",neighbors_y)
+    C = (set.intersection(set(neighbors_x),set(neighbors_y)))
+    N = (set.union(set(neighbors_x),set(neighbors_y)))
+    print(N)
+    print(C)
+    UCX = neighbors_x - C
+    UCY = neighbors_y - C
+    print(UCX)
+    Score = (len(C)/len(N)) 
+    print(Score)
