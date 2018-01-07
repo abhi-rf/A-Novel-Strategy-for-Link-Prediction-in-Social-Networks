09808e8d29c159e4f00c692d9ed43008dcfd5d91
Avichal Rakesh <avichal.rakesh@gmail.com>
2018-01-07 22:56:01 +0530

    start calculating part 2 of score

--- impl.py
+++ impl.py
@@ -4,32 +4,43 @@ from pprint import pprint
 
 dolphin = "./dolphins.gml"
 karate = "./karate.gml"
+illustration = './illustration.gml'
 
 if __name__ == "__main__":
     #Finding prob between 0 and 5
 
     # G = nx.read_gml(dolphin, label='id')
-    G = nx.read_gml(karate, label='id')
+    # G = nx.read_gml(karate, label='id')
+    G = nx.read_gml(illustration, label='id')
 
     # pprint(G.nodes())
     # print(list(G.neighbors(0)))
 
-    x = 1
+    x = 2
     y = 5
 
     neighbors_x = set(G.neighbors(x))
     neighbors_y = set(G.neighbors(y))
-    # print(neighbors_x, neighbors_y)
+    pprint(neighbors_x)
+    pprint(neighbors_y)
 
-    C = set.intersection(neighbors_x, neighbors_y)
     N = set.union(neighbors_x, neighbors_y)
-    # pprint(N)
-    # pprint(C)
+    C = set.intersection(neighbors_x, neighbors_y)
+    pprint(N)
+    pprint(C)
 
     UCX = neighbors_x - C
     UCY = neighbors_y - C
-    # print(UCX)
-    # print(UCY)
+    print(UCX)
+    print(UCY)
 
-    # Score = (len(C)/len(N))
+    conn_UCN = 0
+    for ucx in UCX:
+        for ucy in UCY:
+            if G.has_edge(ucx, ucy):
+                conn_UCN += 1
+
+    print(conn_UCN)
+
+    # Score = (len(C))
     # print(Score)
