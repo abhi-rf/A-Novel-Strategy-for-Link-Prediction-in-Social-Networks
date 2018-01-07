88550dfdd40269e14d5deaa8feb1c4dddf5ad4d0
Avichal Rakesh <avichal.rakesh@gmail.com>
2018-01-07 22:09:40 +0530

    code cleanup

--- impl.py
+++ impl.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  6 23:08:40 2018
-
-@author: Dell
-"""
-
 import networkx as nx
+from pprint import pprint
+
 dolphin = "./dolphins.gml"
 karate = "./karate.gml"
 
 if __name__ == "__main__":
     #Finding prob between 0 and 5
-    G = nx.read_gml(dolphin, label='id')
-    #G = nx.read_gml(karate,label='id')
-    #print(G.nodes())
-    #print(list(G.neighbors(0)))
-    neighbors_x = set((G.neighbors(0)))
-    neighbors_y = set((G.neighbors(5)))
-    print(neighbors_x," ",neighbors_y)
-    C = (set.intersection(set(neighbors_x),set(neighbors_y)))
-    N = (set.union(set(neighbors_x),set(neighbors_y)))
-    print(N)
-    print(C)
+
+    # G = nx.read_gml(dolphin, label='id')
+    G = nx.read_gml(karate, label='id')
+
+    # pprint(G.nodes())
+    # print(list(G.neighbors(0)))
+
+    x = 1
+    y = 5
+
+    neighbors_x = set(G.neighbors(x))
+    neighbors_y = set(G.neighbors(y))
+    # print(neighbors_x, neighbors_y)
+
+    C = set.intersection(neighbors_x, neighbors_y)
+    N = set.union(neighbors_x, neighbors_y)
+    # pprint(N)
+    # pprint(C)
+
     UCX = neighbors_x - C
     UCY = neighbors_y - C
-    print(UCX)
-    Score = (len(C)/len(N))
-    print(Score)
+    # print(UCX)
+    # print(UCY)
+
+    # Score = (len(C)/len(N))
+    # print(Score)
